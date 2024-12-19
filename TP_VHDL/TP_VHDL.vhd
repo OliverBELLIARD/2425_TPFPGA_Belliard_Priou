848397473b851a883b4209147e70fe736a8078b0
oliver2ba <oliver.belliardabreu@gmail.com>
2024-12-19 12:04:49 +0100

    Blinking LED (50 MHz)

--- TP_VHDL/TP_VHDL.vhd
+++ TP_VHDL/TP_VHDL.vhd
@@ -3,12 +3,22 @@ library ieee;
 
 entity TP_VHDL is
   port (
-    sw  : in  std_logic_vector(3 downto 0);
-    led : out std_logic_vector(3 downto 0)
+    i_clk   : in  std_logic;
+    i_rst_n : in  std_logic;
+    o_led   : out std_logic
   );
 end entity;
 
 architecture rtl of TP_VHDL is
+  signal r_led : std_logic := '0';
 begin
-  led <= sw;
+  process (i_clk, i_rst_n)
+  begin
+    if (i_rst_n = '0') then
+      r_led <= '0';
+    elsif (rising_edge(i_clk)) then
+      r_led <= not r_led;
+    end if;
+  end process;
+  o_led <= r_led;
 end architecture;
